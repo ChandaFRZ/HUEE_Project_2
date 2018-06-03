98684e7e35df1e4b49f41720b90234ec6a0e3139
ChandaFRZ <freeza@gmx.at>
2018-06-03 15:50:52 +0200

    integrate login and registration

--- vue-pwa/src/router/index.js
+++ vue-pwa/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import MessageList from '@/components/MessageList'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
             path: '/msg',
             name: 'MessageList',
             component: MessageList
-        }
+        },
+        {
+          path: '/login',
+          name: 'Login',
+          component: Login
+        },
     ]
 })
