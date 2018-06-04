7a07f54785a169af2f68ab696e0ab6864e91aea4
Theresa <theresaobermayr@gmail.com>
2018-06-04 12:54:16 +0200

    add movie component

--- vue-pwa/src/components/MessageList.vue
+++ vue-pwa/src/components/MessageList.vue
@@ -1,12 +1,22 @@
 <template>
-    <ul>
-        <li v-for="message in messages">
-            {{ message }}
-        </li>
-    </ul>
+    <div>
+        <ul>
+            <li v-for="message in messages">
+                {{ message }}
+            </li>
+        </ul>
+
+        <Movie movieid="1234"></Movie>
+        <Movie movieid="2345"></Movie>
+        <Movie movieid="3456"></Movie>
+        <Movie movieid="4567"></Movie>
+        <Movie movieid="5678"></Movie>
+    </div>
+
 </template>
 
 <script>
+    import Movie from './partials/Movie'
     export default {
       name: 'list',
         data() {
@@ -15,5 +25,8 @@
                 messages: ['Hey John', 'Howdy Paco']
             }
         },
+        components: {
+            Movie
+        }
     }
 </script>
